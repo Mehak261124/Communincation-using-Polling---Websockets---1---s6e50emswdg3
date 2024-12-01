a3c65084770e305c2c0390d2214be594438584df
Mehak261124 <145124929+Mehak261124@users.noreply.github.com>
2024-12-01 13:38:47 +0530

    changes in index.js

--- index.js
+++ index.js
@@ -4,11 +4,33 @@ const port = 3001;
 
 app.use(express.json());
 
+const messages = [];
+
 app.get("/", (_, res) => {
   res.send("Hello World!");
 });
 
-const messages = [];
+app.get("/findMessages", (_, res) => {
+  res.status(200).json({ messages });
+});
+
+app.post("/message", (req, res) => {
+  const { user, text } = req.body;
+
+  if (!user || !text) {
+    return res.status(400).json({ error: "Please provide a valid input" });
+  }
+
+  const message = {
+    user,
+    text,
+    timestamp: new Date().toISOString(),
+  };
+
+  messages.push(message);
+
+  res.status(200).json({ message });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
